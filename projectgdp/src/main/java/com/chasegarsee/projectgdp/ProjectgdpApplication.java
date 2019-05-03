fda2fffc2a23ea6ea3471298203228d8443f37d8
chasegarsee <chasegarseesoftwaredevelopment@gmail.com>
2019-05-03 13:30:29 -0500

    MVP

--- projectgdp/src/main/java/com/chasegarsee/projectgdp/ProjectgdpApplication.java
+++ projectgdp/src/main/java/com/chasegarsee/projectgdp/ProjectgdpApplication.java
@@ -2,7 +2,12 @@ package com.chasegarsee.projectgdp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
+@EnableWebMvc
 @SpringBootApplication
 public class ProjectgdpApplication
 {
@@ -13,6 +18,10 @@ public class ProjectgdpApplication
     {
         countryList = new CountryList();
         SpringApplication.run(ProjectgdpApplication.class, args);
+
+        ApplicationContext ctx = SpringApplication.run(ProjectgdpApplication.class, args);
+        DispatcherServlet dispatcherServlet = (DispatcherServlet) ctx.getBean("dispatcherServlet");
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
--- projectgdp/src/main/java/com/chasegarsee/projectgdp/controller/GDPController.java
+++ projectgdp/src/main/java/com/chasegarsee/projectgdp/controller/GDPController.java
@@ -3,23 +3,30 @@ package com.chasegarsee.projectgdp.controller;
 import com.chasegarsee.projectgdp.ProjectgdpApplication;
 import com.chasegarsee.projectgdp.exception.ResourceNotFoundException;
 import com.chasegarsee.projectgdp.model.GDP;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import org.slf4j.Logger;
 import java.util.ArrayList;
 import java.util.Comparator;
 
 @RestController
 public class GDPController
 {
+
+    private static final Logger logger = LoggerFactory.getLogger(GDP.class);
 //      /names - return using the JSON format all of the countries alphabetized by name
         //localhost:1992/names/
     @GetMapping(value = "/names", produces = {"application/json"})
     public ResponseEntity<?> allCountries ()
     {
+        logger.info("GOT ALL COUNTRIES");
         return new ResponseEntity<>(ProjectgdpApplication.countryList.gdpList, HttpStatus.OK);
     }
 
@@ -29,8 +36,9 @@ public class GDPController
     public ResponseEntity<?> countriesByGDPDecending()
     {
         ArrayList<GDP> gdpList = ProjectgdpApplication.countryList.gdpList;
-        gdpList.sort(Comparator.comparing(GDP::getGdp));
+        gdpList.sort(Comparator.comparingLong(GDP::getGdp));
 
+        logger.info("COUNTRIES SORTED");
         return new ResponseEntity<>(gdpList, HttpStatus.OK);
     }
 
@@ -46,7 +54,7 @@ public class GDPController
         if (rtnGdp == null) {
             throw new ResourceNotFoundException("Couldn't find a Country named: " + name);
         }
-
+        logger.info(name + " ACCESSED");
         return new ResponseEntity<>(rtnGdp, HttpStatus.OK);
     }
 
@@ -61,6 +69,7 @@ public class GDPController
             {
                 throw new ResourceNotFoundException("Country with this id " + id + " NOT FOUND");
             }
+            logger.info("COUNTRY #" + id + " ACCESSED");
             return new ResponseEntity<>(rtnGDP, HttpStatus.OK);
         }
     }
@@ -77,6 +86,20 @@ public class GDPController
         GDP rtnGdp = gdpList.get(gdpList.size() / 2);
 
         return new ResponseEntity<>(rtnGdp, HttpStatus.OK);
+    }
+
+    @GetMapping(value="/economy/table")
+    public ModelAndView getCountriesTableGreaterThanGDP()
+    {
+
+        ArrayList<GDP> gdpList = ProjectgdpApplication.countryList.gdpList;
+        gdpList.sort(Comparator.comparingLong(GDP::getGdp));
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("countries");
+        mav.addObject("gdpList", gdpList);
+
+        return mav;
     }
 
 }
